7e70f6f13cec34506d61b846d12cd93fb097921b
RamziBach <ramzibach.dev@gmail.com>
2021-07-09 14:38:43 -0400

    mobile nav

--- src/components/global/header/index.js
+++ src/components/global/header/index.js
@@ -1,47 +1,64 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import MobileNav from '../mobile_nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from './header.module.css';
 
 const Header = ({ price }) => {
+  const [isMobileDisplayOpen, setIsMobileDisplayOpen] = useState(false);
+
+  const handleMobileClick = () =>
+    setIsMobileDisplayOpen(prevState => !prevState);
+
+  useEffect(() => {
+    console.log('Website developed by Ramzi Bach - www.ramzibach.com');
+  }, []);
+
   return (
-    <div data-scroll-section>
-      <header className={styles.header}>
-        <div className={styles.container}>
-          <div className={styles.parent}>
-            <div className={styles.titleContainer}>
-              <h2 className={styles.title}>wax money</h2>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.parent}>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>wax money</h2>
+          </div>
+          <div className={styles.menuContainer}>
+            <nav>
+              <ul className={styles.menu}>
+                <li>
+                  <button className={styles.navBtn}>home</button>
+                </li>
+                <li>
+                  <button className={styles.navBtn}>store</button>
+                </li>
+                <li>
+                  <button className={styles.navBtn}>vip</button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.tickerContainer}>
+            <div className={styles.imgContainer}>
+              <Image src="/token.jpg" alt="token" layout="fill" />
             </div>
-            <div className={styles.menuContainer}>
-              <nav>
-                <ul className={styles.menu}>
-                  <li>
-                    <button className={styles.navBtn}>home</button>
-                  </li>
-                  <li>
-                    <button className={styles.navBtn}>store</button>
-                  </li>
-                  <li>
-                    <button className={styles.navBtn}>vip</button>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className={styles.tickerContainer}>
-              <div className={styles.imgContainer}>
-                <Image src="/token.jpg" alt="token" layout="fill" />
-              </div>
-              <a
-                href="https://rally.io/creator/WAXM/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.ticker}
-              >
-                ${price}
-              </a>
-            </div>
+            <a
+              href="https://rally.io/creator/WAXM/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.ticker}
+            >
+              ${price}
+            </a>
+          </div>
+          <div className={styles.mobileContainer}>
+            <button onClick={handleMobileClick}>
+              <FontAwesomeIcon icon={faBars} className={styles.i} />
+            </button>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+      <MobileNav price={price} display={isMobileDisplayOpen} />
+    </header>
   );
 };
 
---
+++ src/components/global/mobile_nav/index.js
@@ -0,0 +1,35 @@
+import styles from './mobileNav.module.css';
+import Image from 'next/image';
+
+const MobileNav = ({ price, display }) => {
+  return (
+    <div
+      style={display ? { display: 'flex' } : { display: 'none' }}
+      className={styles.mobilenav}
+    >
+      <div className={styles.container}>
+        <div className={styles.parent}>
+          <a className={styles.btn}>home</a>
+          <a className={styles.btn}>store</a>
+          <a className={styles.btn}>vip</a>
+          <div className={styles.waxCoin} aria-label="Waxm coin price">
+            <div className={styles.logo}>
+              <Image src="/token.jpg" alt="token" layout="fill" />
+            </div>
+            <a
+              href="https://rally.io/creator/WAXM/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.price}
+              title="$WAXM Coin Price"
+            >
+              ${price}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
