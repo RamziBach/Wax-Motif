298afaef4e1345a81a56f3e4009c3a93709dfa20
RamziBach <ramzibach.dev@gmail.com>
2021-07-11 14:30:59 -0400

    socials links

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -24,31 +24,85 @@ const Footer = () => {
             height="100"
           />
           <div className={styles.socialsContainer}>
-            <a>
+            <a
+              href="https://soundcloud.com/waxmotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Soundcloud"
+              aria-label="Soundcloud"
+            >
               <FontAwesomeIcon icon={faSoundcloud} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://discord.com/invite/SapxcWJd"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Discord"
+              aria-label="Discord"
+            >
               <FontAwesomeIcon icon={faDiscord} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://open.spotify.com/artist/7zm3aSdmGiOkTt0aZFSO8R?si=WYtSja7_QbSXMclHA11HMA&nd=1"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Spotify"
+              aria-label="Spotify"
+            >
               <FontAwesomeIcon icon={faSpotify} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://www.youtube.com/user/Waxmotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Youtube"
+              aria-label="Youtube"
+            >
               <FontAwesomeIcon icon={faYoutube} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://twitter.com/WaxMotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Twitter"
+              aria-label="Twitter"
+            >
               <FontAwesomeIcon icon={faTwitter} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://www.instagram.com/waxmotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Instagram"
+              aria-label="Instagram"
+            >
               <FontAwesomeIcon icon={faInstagram} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://www.facebook.com/WaxMotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Facebook"
+              aria-label="Facebook"
+            >
               <FontAwesomeIcon icon={faFacebook} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://music.apple.com/us/artist/wax-motif/300682465"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Apple Music"
+              aria-label="Apple Music"
+            >
               <FontAwesomeIcon icon={faApple} className={styles.i} />
             </a>
-            <a>
+            <a
+              href="https://www.twitch.tv/waxmotif"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Twitch"
+              aria-label="Twitch"
+            >
               <FontAwesomeIcon icon={faTwitch} className={styles.i} />
             </a>
           </div>
